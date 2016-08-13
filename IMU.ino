6dfba169d7779573f4ae4170e57a1f600e4c35fb
chenset <4199191@qq.com>
2016-08-13 16:24:39 +0800

    daily

--- IMU.ino
+++ IMU.ino
@@ -8,7 +8,7 @@ void IMU_fillter(void)
   gyroY = (i2cData[10] << 8) | i2cData[11];
   gyroZ = (i2cData[12] << 8) | i2cData[13];
 
-  angle_PID_compute();
+  // angle_PID_compute();
 
   double dt = (double)(micros() - timer) / 1000000;
   timer = micros();
@@ -16,25 +16,25 @@ void IMU_fillter(void)
   double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
   double gyroYrate = gyroY / 131.0;
 
-  angle_PID_compute();
+  // angle_PID_compute();
 
   gyroYangle += gyroYrate * dt;
   compAngleY = 0.93 * (compAngleY + gyroYrate * dt) + 0.07 * pitch;
-  if (abs(compAngleY - pre_angle.data) > 15)
-    FLAG |= FALLDOWN;
+  // if (abs(compAngleY - pre_angle.data) > 15)
+    // FLAG |= FALLDOWN;
 
 #ifdef IMU_OUTPUT
   Serial.println(compAngleY);
 #endif
 }
 
-void write_eeprom(double)
-{
-  uint8_t  *p;
-  p = pre_angle.dchar;
-  for (char i = 0; i < 4; i++)
-  {
-    EEPROM.write(i, *p);
-    p++;
-  }
-}
+// void write_eeprom(double)
+// {
+//   uint8_t  *p;
+//   p = pre_angle.dchar;
+//   for (char i = 0; i < 4; i++)
+//   {
+//     EEPROM.write(i, *p);
+//     p++;
+//   }
+// }
--- balance-car.ino
+++ balance-car.ino
@@ -3,21 +3,25 @@
 #include <Adafruit_GFX.h>
 #include <EEPROM.h>
 #include <Wire.h>
-//#include "Camera.h"
-// #include "Bitmap.h"
 #include "MotorDC.h"
-// #include <Encoder.h>
 #include <PID_v1.h>
-#define IMU_OUTPUT;
+#include "I2Cdev.h"
+
 MotorDC motor1(5,8);
 MotorDC motor2(6,7);
-#define NONSTART 0x0001    //启动标志
-#define TURNING 0x0002     //转向中标志
-#define FALLDOWN 0x0004    //摔倒标志
-#define COMDONE 0x0020     //帧数据结束标志
 
+// PID
 double angleSetpoint, angleInput, angleOutput;
-PID anglePID(&angleInput, &angleOutput, &angleSetpoint,30,250,0.50, DIRECT);//REVERSE DIRECT
+double angleP = 10, angleI = 100,  angleD = 0.2;
+PID anglePID(&angleInput, &angleOutput, &angleSetpoint,angleP,angleI,angleD, DIRECT);//REVERSE DIRECT
+
+// 实例化SPI OLED
+#define OLED_MOSI   9
+#define OLED_CLK   10
+#define OLED_DC    11
+#define OLED_RESET 13
+
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, -1);
 
 /***************MPU6050变量定义**********************/
 double accX, accY, accZ;
@@ -27,56 +31,10 @@ int16_t tempRaw;
 uint32_t timer;
 uint8_t i2cData[14];
 
-/*********************其他变量**********************/
-uint32_t FLAG , CAM_timer , SONIC_timer ;
-float distance_cm, joy_x, joy_y;
-char comdata[19], data_p; //串口缓存数据
-union EEPROM_DATA {       //联合体用于分段储存EEPROM数据
-  double data;
-  uint8_t dchar[4];
-} pre_angle;
 
-
-/***************PID变量定义**********************/
-#ifdef PARAM_DEBUG  //角度环数据
-double  P_angle = 0, I_angle = 0, D_angle = 0;
-#else
-#define P_angle  0
-#define I_angle  0
-#define D_angle  0
-#endif
-double  angle_setpoint = 0, angle_output, angle_integral;
-uint32_t angle_PID_timer;
-
-#ifdef PARAM_DEBUG  //速度环数据
-double  P_speed = 0, I_speed = 0;
-#else
-#define P_speed  0
-#define I_speed  0
-#endif
-double  speed_setpoint = 0, speed_output, speed_integral;
-uint32_t speed_PID_timer;
-
-#ifdef PARAM_DEBUG  //方向环数据
-double  P_turn = 0, I_turn = 0;
-#else
-#define P_turn  0
-#define I_turn  0
-#endif
-double  turn_integral = 0 , turn_output ;
-
-//OLED
-#define OLED_MOSI   9
-#define OLED_CLK   10
-#define OLED_DC    11
-#define OLED_RESET 13
-//oled spi 实例化OLED
-Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, -1);
-
+// 函数声明
+void (* resetFunc) (void) = 0;
 void IMU_fillter(void);           //IMU角度融合
-void angle_PID_compute(void);     //角度环计算
-void speed_PID_compute(void);     //速度环计算
-void write_eeprom(double);        //写入EEPROM
 
 void setup()
 {
@@ -93,6 +51,7 @@ void setup()
 
 	delay(500);
 
+	/*********************MPU6050初始化********************/
 	i2cData[0] = 7;
 	i2cData[1] = 0x00;
 	i2cData[2] = 0x00;
@@ -102,7 +61,7 @@ void setup()
 	while (i2cRead(0x75, i2cData, 1));
 	if (i2cData[0] != 0x68)
 	while (1);
-		delay(100);
+	delay(100);
 
 	while (i2cRead(0x3B, i2cData, 6));
 	accX = (i2cData[0] << 8) | i2cData[1];
@@ -114,46 +73,40 @@ void setup()
 	gyroYangle = pitch;
 	compAngleY = pitch;
 
-	FLAG |= NONSTART;
+	timer = micros();
 
-	angleSetpoint = atan2(accX, accZ) * RAD_TO_DEG + 180.0;
 
-  	// anglePID.SetMode(AUTOMATIC);
-  
-
+	angleInput = atan2(accX, accZ) * RAD_TO_DEG + 180.0;
+  	angleSetpoint = 180;
+  	anglePID.SetOutputLimits(-254,254);
+  	anglePID.SetSampleTime(10);
+  	anglePID.SetMode(AUTOMATIC);
+	// 电机停机
   	motor1.setSpeed(0);
   	motor2.setSpeed(0);
 }
 
 void loop()
 {
-	IMU_fillter();//3ms
-  	angle_PID_compute();
+
+	angleInput = atan2(accX, accZ) * RAD_TO_DEG + 180.0;
+	anglePID.SetTunings(angleP, angleI, angleD);      // * While most users will set the tunings once in the 
+	anglePID.Compute();
 
 	display.clearDisplay();
 	display.setTextSize(1);
 	display.setCursor(0, 0);
 
-  	display.println(atan2(accX, accZ) * RAD_TO_DEG + 180.0);
-  	angleInput = atan2(accX, accZ) * RAD_TO_DEG + 180.0;
-
-  //Setpoint = 22.0;
-  // myPID.SetTunings(kp,ki,kd);
-
-  	display.println(micros());
-
-	// anglePID.SetOutputLimits(-254,254);
-  	// anglePID.SetSampleTime(5);
-  // myPID.SetMode(AUTOMATIC);
-  	// anglePID.Compute();
-
-  	display.println(angleOutput);
-
-
+	display.println(atan2(accX, accZ) * RAD_TO_DEG + 180.0);
+	display.println(angleOutput);
+	display.println(angleP,angleI,angleD);
   	display.display();
 
-	// motor1.setSpeed(angleOutput);
-	// motor2.setSpeed(-angleOutput);
+	motor1.setSpeed(angleOutput);
+	motor2.setSpeed(-angleOutput);
 
+	// resetFunc();
+	// delay(10000);
 
+  	IMU_fillter();//3ms
 }
